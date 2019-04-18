45d7de3d48fd34730e36c329c748e0df1a497696
Zhi Wei <zhiwei.lee@nidec.com>
2019-04-18 11:15:47 +0800

    Moved helper functions into modules

---
+++ helper.py
@@ -0,0 +1,162 @@
+# Helper functions to read and preprocess data files in Matlab format
+# Data science libraries
+import scipy
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+# Others
+from IPython.core.debugger import set_trace
+from pathlib import Path
+
+def matfile_to_dic(folder_path):
+    '''
+    Read all the matlab files of the CWRU Bearing Dataset and return a dictionary.
+    The key of each item is the filename and the value is the data of one matlab file, 
+    which also has key value pairs.
+    
+    Parameter:
+        folder_path: 
+            Path (Path object) of the folder which contains the matlab files.
+    Return:
+        output_dic: 
+            Dictionary which contains data of all files in the folder_path.
+    '''
+    output_dic = {}
+    for _, filepath in enumerate(folder_path.glob('*.mat')):
+        key_name = str(filepath).split('\\')[-1]    #strip the folder path and get the filename only.
+        output_dic[key_name] = scipy.io.loadmat(filepath)
+    return output_dic
+
+def remove_dic_items(dic):
+    '''
+    Remove redundant data in the dictionary returned by matfile_to_dic inplace.
+    '''
+    # For each file in the dictionary, delete the redundant key-value pairs
+    for _, values in dic.items():
+        del values['__header__']
+        del values['__version__']    
+        del values['__globals__']
+        
+def rename_keys(dic):
+    '''
+    Rename some keys so that they can be loaded into a 
+    DataFrame with consistent column names
+    '''
+    # For each file in the dictionary
+    for _,v1 in dic.items():
+        # For each key-value pair, rename the following keys 
+        for k2,_ in list(v1.items()):
+            if 'DE_time' in k2:
+                v1['DE_time'] = v1.pop(k2)
+            elif 'BA_time' in k2:
+                v1['BA_time'] = v1.pop(k2)
+            elif 'FE_time' in k2:
+                v1['FE_time'] = v1.pop(k2)
+            elif 'RPM' in k2:
+                v1['RPM'] = v1.pop(k2)
+                
+def label(filename):
+    '''
+    Create label for each signal based on the filename.
+    Usage:
+        df['label'] = df['filename'].apply(label)
+    '''
+    if 'B' in filename:
+        return 'B'
+    elif 'IR' in filename:
+        return 'IR'
+    elif 'OR' in filename:
+        return 'OR'
+    elif 'Normal' in filename:
+        return 'N'
+    
+def matfile_to_df(folder_path):
+    '''
+    Read all the matlab files in the folder, preprocess, and return a DataFrame
+    
+    Parameter:
+        folder_path: 
+            Path (Path object) of the folder which contains the matlab files.
+    Return:
+        DataFrame with preprocessed data
+    '''
+    dic = matfile_to_dic(folder_path)
+    remove_dic_items(dic)
+    rename_keys(dic)
+    df = pd.DataFrame.from_dict(dic).T
+    df = df.reset_index().rename(mapper={'index':'filename'},axis=1)
+    df['label'] = df['filename'].apply(label)
+    return df.drop(['BA_time','FE_time', 'RPM', 'ans'], axis=1, errors='ignore')
+
+def divide_signal(df, segment_length):
+    '''
+    This function divide the signal into segments, each with a specific number of points as
+    defined by segment_length. Each segment will be added as an example (a row) in the 
+    returned DataFrame. Thus it increases the number of training examples. The remaining 
+    points which are less than segment_length are discarded.
+    
+    Parameter:
+        df: 
+            DataFrame returned by matfile_to_df()
+        segment_length: 
+            Number of points per segment.
+    Return:
+        DataFrame with segmented signals and their corresponding filename and label
+    '''
+    dic = {}
+    idx = 0
+    for i in range(df.shape[0]):
+        n_sample_points = len(df.iloc[i,1])
+        n_segments = n_sample_points // segment_length
+        for segment in range(n_segments):
+            dic[idx] = {
+                'signal': df.iloc[i,1][segment_length * segment:segment_length * (segment+1)], 
+                'label': df.iloc[i,2],
+                'filename' : df.iloc[i,0]
+            }
+            idx += 1
+    df_tmp = pd.DataFrame.from_dict(dic,orient='index')
+    
+    return pd.concat([df_tmp[['label', 'filename']], pd.DataFrame(np.hstack(df_tmp["signal"].values).T)], axis=1 )
+
+def normalize_signal(df):
+    '''
+    Normalize the signals in the DataFrame returned by matfile_to_df() by subtracting
+    the mean and dividing by the standard deviation.
+    '''
+    mean = df['DE_time'].apply(np.mean)
+    std = df['DE_time'].apply(np.std)
+    df['DE_time'] = (df['DE_time'] - mean) / std
+
+def get_df_all(normal_path, DE_path, segment_length=512, normalize=False):
+    '''
+    Load, preprocess and return a DataFrame which contains all signals data and
+    labels and is to be used for model training.
+    
+    Parameter:
+        normal_path: 
+            Path of the folder which contains matlab files of normal bearings
+        DE_path: 
+            Path of the folder which contains matlab files of DE faulty bearings
+        segment_length: 
+            Number of points per segment. See divide_signal() function
+        normalize: 
+            Boolean to perform normalization to the signal data
+    Return:
+        df_all: 
+            DataFrame which is ready to be used for model training.
+    '''
+    df_Normal = matfile_to_df(normal_path)
+    df_DE = matfile_to_df(DE_path)
+
+    if normalize:
+        normalize_signal(df_Normal)
+        normalize_signal(df_DE)
+    df_normal_processed = divide_signal(df_Normal, segment_length)
+    df_faulty_processed = divide_signal(df_DE, segment_length)
+    df_all = pd.concat([df_normal_processed, df_faulty_processed], axis=0, ignore_index=True)
+    map_label = {'N':0, 'B':1, 'IR':2, 'OR':3}
+    df_all['label'] = df_all['label'].map(map_label)
+    return df_all
---
+++ train_helper.py
@@ -0,0 +1,83 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+from torch import Tensor
+from torch.utils.data import TensorDataset, DataLoader
+from torch import optim
+from torch.nn.modules.loss import CrossEntropyLoss
+
+from one_cycle import OneCycle, update_lr, update_mom
+
+# Functions for training
+def get_dataloader(train_ds, valid_ds, bs):
+    return (
+        DataLoader(train_ds, batch_size=bs, shuffle=True),
+        DataLoader(valid_ds, batch_size=bs * 2),
+    )
+
+def loss_batch(model, loss_func, xb, yb, opt=None):
+    loss = loss_func(model(xb), yb)
+
+    if opt is not None:
+        loss.backward()
+        opt.step()
+        opt.zero_grad()
+
+    return loss.item(), len(xb)
+
+def fit(epochs, model, loss_func, opt, train_dl, valid_dl):
+    print('EPOCH', '\t', 'Val Loss', '\t', 'Accuracy')
+    train_losses = []
+    val_losses = []
+    for epoch in range(epochs):
+        # Train
+        model.train()
+        mean_loss = 0.0
+        for xb, yb in train_dl:
+            loss, batch_size = loss_batch(model, loss_func, xb, yb, opt)
+            mean_loss += loss/batch_size
+        train_losses.append(mean_loss)
+        
+        # Validate
+        model.eval()
+        mean_loss = 0.0
+        predictions = []
+        with torch.no_grad():
+            for xb, yb in valid_dl: 
+                loss, batch_size = loss_batch(model, loss_func, xb, yb)
+                mean_loss += loss/batch_size
+                predictions.append(torch.argmax(model(xb), dim=1))
+                
+        val_losses.append(mean_loss)
+        predictions = torch.cat(predictions, dim=0).numpy()
+
+        accuracy = np.mean((predictions == valid_dl.dataset.tensors[1].numpy()))
+
+        print(f'{epoch}: \t', f'{mean_loss:.05f}', '\t', f'{accuracy:.05f}')
+        
+    return model, train_losses, val_losses
+
+def fit_one_cycle(epochs, model, loss_func, opt, train_dl, valid_dl, one_cycle_scheduler):
+    print('EPOCH', '\t', 'Val Loss', '\t', 'Accuracy')
+    for epoch in range(epochs):
+        model.train()
+        for xb, yb in train_dl:
+            loss_batch(model, loss_func, xb, yb, opt)
+            lr, mom = onecycle.calc()
+            update_lr(opt, lr)
+            update_mom(opt, mom)
+
+        model.eval()
+        with torch.no_grad():
+            loss = [loss_func(model(xb), yb) for xb, yb in valid_dl]
+            loss = torch.stack(loss, dim=0).numpy()
+            predictions = [torch.argmax(model(xb), dim=1) for xb, yb in valid_dl]
+#             set_trace()
+            predictions = torch.cat(predictions, dim=0).numpy()
+#         set_trace()
+        val_loss = np.mean(loss)
+        accuracy = np.mean((predictions == valid_dl.dataset.tensors[1].numpy()))
+
+        print(f'{epoch}: \t', f'{val_loss:.05f}', '\t', f'{accuracy:.05f}')
+        
+    return model
