533b47152a90a73eec695d9726f271cd8e79c720
Zhi Wei <zhiwei.lee@nidec.com>
2019-04-22 11:19:34 +0800

    Refactored loss_batch, validate, added train loss

--- train_helper.py
+++ train_helper.py
@@ -6,6 +6,7 @@ from torch.utils.data import TensorDataset, DataLoader
 from torch import optim
 from torch.nn.modules.loss import CrossEntropyLoss
 
+from sklearn.metrics import accuracy_score
 import numpy as np
 from one_cycle import OneCycle, update_lr, update_mom
 
@@ -51,14 +52,16 @@ def loss_batch(model, loss_func, xb, yb, opt=None):
             len(xb): Int
                 Number of examples of the current batch
     '''
-    loss = loss_func(model(xb), yb)
+    out = model(xb)
+    loss = loss_func(out, yb)
+    pred = torch.argmax(out, dim=1).numpy()
 
     if opt is not None:
         loss.backward()
         opt.step()
         opt.zero_grad()
 
-    return loss.item(), len(xb)
+    return loss.item(), len(xb), pred
 
 def fit(epochs, model, loss_func, opt, train_dl, valid_dl, one_cycle=None):
     '''
@@ -92,37 +95,62 @@ def fit(epochs, model, loss_func, opt, train_dl, valid_dl, one_cycle=None):
             val_losses: List
                 List of the validation loss at each epochs.
     '''
-    print('EPOCH', '\t', 'Val Loss', '\t', 'Accuracy')
+    print(
+        'EPOCH', '\t', 
+        'Train Loss', '\t',
+        'Val Loss', '\t', 
+        'Train Accuracy', '\t',
+        'Val Accuracy', '\t')
     train_losses = []
+    train_accuracies = []
     val_losses = []
+    val_accuracies = []
     for epoch in range(epochs):
         # Train
         model.train()
-        mean_loss = 0.0
+        train_loss = 0.0
+        train_accuracy = 0.0
+        batch_count = 0
         for xb, yb in train_dl:
-            loss, batch_size = loss_batch(model, loss_func, xb, yb, opt)
-            mean_loss += loss/batch_size
+            loss, batch_size, pred = loss_batch(model, loss_func, xb, yb, opt)
+            train_loss += loss/batch_size
+            train_accuracy += accuracy_score(yb.numpy(), pred)
             
             if one_cycle:
                 lr, mom = one_cycle.calc()
                 update_lr(opt, lr)
                 update_mom(opt, mom)
-        train_losses.append(mean_loss)
-        
+            batch_count += 1
+
+        train_losses.append(train_loss)
+        train_accuracy /= batch_count
+        train_accuracies.append(train_accuracy)
+
         # Validate
         model.eval()
-        mean_loss = 0.0
-        predictions = []
         with torch.no_grad():
-            for xb, yb in valid_dl: 
-                loss, batch_size = loss_batch(model, loss_func, xb, yb)
-                mean_loss += loss/batch_size
-                predictions.append(torch.argmax(model(xb), dim=1))
+            val_loss, val_accuracy = validate(model, valid_dl, loss_func)
                 
-        val_losses.append(mean_loss)
-        predictions = torch.cat(predictions, dim=0).numpy()
-        accuracy = np.mean((predictions == valid_dl.dataset.tensors[1].numpy()))
-
-        print(f'{epoch}: \t', f'{mean_loss:.05f}', '\t', f'{accuracy:.05f}')
+        val_losses.append(val_loss)
+        val_accuracies.append(val_accuracy)
         
-    return model, train_losses, val_losses
+
+        print(
+            f'{epoch}: \t', 
+            f'{train_loss:.05f}', '\t',
+            f'{val_loss:.05f}', '\t', 
+            f'{train_accuracy:.05f}', '\t'
+            f'{val_accuracy:.05f}', '\t')
+        
+    return model, train_losses, val_losses, val_accuracies
+
+def validate(model, dl, loss_func):
+    mean_loss = 0.0
+    predictions = []
+    for xb, yb in dl: 
+        loss, batch_size, pred = loss_batch(model, loss_func, xb, yb)
+        mean_loss += loss/batch_size
+        predictions.append(pred)
+    predictions = np.concatenate(predictions, axis=0)
+    accuracy = np.mean((predictions == dl.dataset.tensors[1].numpy()))
+    return mean_loss, accuracy
