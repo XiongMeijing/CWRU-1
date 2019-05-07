12cc558b4758ed0996a45d34bea85bb33d0cce38
Zhi Wei <zhiwei.lee@nidec.com>
2019-05-07 13:17:16 +0800

    Refactored StartPageController
    - moved StartPageController into a new module controller.py

---
+++ gui_core/controller.py
@@ -0,0 +1,62 @@
+from gui_core.model import Model
+from tkinter import *
+
+class StartPageController:
+    def __init__(self, page):
+        self.page = page
+        self.model = Model()
+        self.page.button_open.config(command=self.select_files)
+        self.page.button_del_all.config(command=self.delete_list_all)
+        self.page.button_del_selected.config(command=self.delete_list_selected)
+        self.page.button_pred.config(command=self.get_predictions)
+        self.page.plotframe.draw_button.config(command=self.plot_something)
+
+    def select_files(self):
+        paths = filedialog.askopenfilenames()
+        filenames = self.model.read_files(paths)
+        self.page.labelframe.lb_filenames.delete(0,END)
+        self.page.labelframe.lb_predictions.delete(0,END)
+        for i, (filename, pred) in enumerate(zip(self.model.data['filenames'], self.model.data['prediction'])):
+            self.page.labelframe.lb_filenames.insert(i, filename)
+            self.page.labelframe.lb_predictions.insert(i, pred)
+
+    def delete_list_all(self):
+        for k,v in self.model.data.items():
+            self.model.data[k] = []
+        self.page.labelframe.lb_filenames.delete(0, END)
+        self.page.labelframe.lb_predictions.delete(0, END)
+
+    def delete_list_selected(self):
+        # Each time listbox.delete method is called, the index of the listbox is
+        # reset. Hence the index of the selected item needs to be corrected by
+        # subtracting the count of deleted items.
+        del_indices = self.get_list_selection()
+        count = 0
+        for del_idx in del_indices:
+            for k,v in self.model.data.items():
+                v.pop(del_idx - count)
+            
+            self.page.labelframe.lb_filenames.delete(del_idx - count)
+            self.page.labelframe.lb_predictions.delete(del_idx - count)
+            count += 1
+
+    def get_list_selection(self):
+        return self.page.labelframe.lb_filenames.curselection()
+
+    def plot_something(self):
+        try:
+            idx = self.get_list_selection()
+            idx = idx[0]
+            arr = self.model.data['signals'][idx]
+            self.page.plotframe.a.clear()
+            self.page.plotframe.a.plot(arr)
+            self.page.plotframe.canvas.draw()
+        except Exception as e:
+            print(e)
+
+    def get_predictions(self):
+        pred_indices = self.get_list_selection()
+        for pred_index in pred_indices:
+            self.model.predict(pred_index)
+            self.page.labelframe.lb_predictions.insert(pred_index, self.model.data['prediction'][pred_index])
+            self.page.labelframe.lb_predictions.delete(pred_index + 1)
--- gui_tk.py
+++ gui_tk.py
@@ -1,6 +1,7 @@
-from gui_core.view import *
-from gui_core.model import Model
-
+import tkinter as tk
+from tkinter import *
+from gui_core.view import StartPage
+from gui_core.controller import StartPageController
 from pathlib import Path
 
 class MainApp(tk.Tk):
@@ -9,15 +10,11 @@ class MainApp(tk.Tk):
         
         tk.Tk.__init__(self, *args, **kwargs)
         container = tk.Frame(self)
-
         container.pack(side="top", fill="both", expand = True)
-
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
-
         
         self.frames = {}
-
         for F in [StartPage]:
             frame = F(container, self)
             self.frames[F] = frame
@@ -31,70 +28,6 @@ class MainApp(tk.Tk):
         frame.tkraise()
 
 
-class StartPageController:
-    def __init__(self, page):
-        self.page = page
-        self.model = Model()
-        self.page.button_open.config(command=self.select_files)
-        self.page.button_del_all.config(command=self.delete_list_all)
-        self.page.button_del_selected.config(command=self.delete_list_selected)
-        self.page.button_pred.config(command=self.make_prediction)
-        self.page.plotframe.draw_button.config(command=self.plot_something)
-
-    def select_files(self):
-        paths = filedialog.askopenfilenames()
-        filenames = self.model.read_files(paths)
-        self.page.labelframe.lb_filenames.delete(0,END)
-        self.page.labelframe.lb_predictions.delete(0,END)
-        for i, (filename, pred) in enumerate(zip(self.model.data['filenames'], self.model.data['prediction'])):
-            self.page.labelframe.lb_filenames.insert(i, filename)
-            self.page.labelframe.lb_predictions.insert(i, pred)
-
-    def delete_list_all(self):
-        for k,v in self.model.data.items():
-            self.model.data[k] = []
-        self.page.labelframe.lb_filenames.delete(0, END)
-        self.page.labelframe.lb_predictions.delete(0, END)
-
-    def delete_list_selected(self):
-        # Each time listbox.delete method is called, the index of the listbox is
-        # reset. Hence the index of the selected item needs to be corrected by
-        # subtracting the count of deleted items.
-        del_indices = self.get_list_selection()
-        count = 0
-        for del_idx in del_indices:
-            for k,v in self.model.data.items():
-                v.pop(del_idx - count)
-            
-            self.page.labelframe.lb_filenames.delete(del_idx - count)
-            self.page.labelframe.lb_predictions.delete(del_idx - count)
-            count += 1
-
-    def get_list_selection(self):
-        return self.page.labelframe.lb_filenames.curselection()
-
-    def plot_something(self):
-        try:
-            idx = self.get_list_selection()
-            idx = idx[0]
-            arr = self.model.data['signals'][idx]
-            self.page.plotframe.a.clear()
-            self.page.plotframe.a.plot(arr)
-            self.page.plotframe.canvas.draw()
-        except Exception as e:
-            print(e)
-
-    def make_prediction(self):
-        pred_indices = self.get_list_selection()
-        for pred_index in pred_indices:
-            self.model.predict(pred_index)
-            self.page.labelframe.lb_predictions.insert(pred_index, self.model.data['prediction'][pred_index])
-            self.page.labelframe.lb_predictions.delete(pred_index + 1)
-
-
-
 if __name__ == "__main__":
     app = MainApp()
     app.mainloop()
-    # testarr = np.array(range(50)).reshape(-1,1)
-    # print(preprocess_signal(testarr, 8))
